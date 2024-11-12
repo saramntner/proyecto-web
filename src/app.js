bce8f947a34cc8865243c341e81d284f3614c7fb
saramntner <smontaner0@uoc.edu>
2024-11-12 20:55:05 +0100

    Subir directorio a git

---
+++ src/app.js
@@ -0,0 +1,56 @@
+//Que los elementos se desvelen al hacer scroll 
+
+const bloques = document.querySelectorAll(".aparecer");
+
+const revelarBloque = (entradas) => {
+    entradas.forEach((entrada) => {
+        if (entrada.isIntersecting) {
+            entrada.target.classList.add("visible");
+        } else {
+            entrada.target.classList.remove("visible");
+        }
+    });
+}
+
+const observador = new IntersectionObserver(revelarBloque, {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0
+});
+
+bloques.forEach((bloque) => observador.observe(bloque));
+
+//Importar dependencia
+import Swal from 'sweetalert2';
+
+//Código de sweetalert2 
+
+document.addEventListener("DOMContentLoaded", () => {
+    const suscribirse = document.getElementById("suscribirse");
+    suscribirse.addEventListener("click", async () => {
+        const { value: email } = await Swal.fire({
+            title: "Suscríbete a la Newsletter",
+            input: "email",
+            inputPlaceholder: "Introduce tu email",
+            confirmButtonText: "Suscribirme",
+            customClass: {
+                popup: 'swalpopup',
+                title: 'swaltitle',
+                input: 'swalinput',
+                validationMessage: 'swalvalidationMessage',
+                confirmButton: 'swalconfirmButton',
+            }
+        });
+        if (email) {
+            Swal.fire({
+                title: "¡Gracias por suscribirte!",
+                customClass: {
+                    popup: 'swalpopup2',
+                    title: 'swaltitle2',
+                    confirmButton: 'swalconfirmButton',
+                }
+            });
+        }
+    })()
+});
+
